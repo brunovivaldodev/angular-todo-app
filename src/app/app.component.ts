e3e473c6ce95eaf3fd46cd9f12a875de473c3d91
Bruno Vivaldo <brunolumeca@live.com>
2021-06-12 10:02:50 +0100

    save itens done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,36 +14,60 @@ export class AppComponent {
   title = 'Todo App';
 
   constructor(private fb : FormBuilder) {
-
     this.form = this.fb.group({
       title : ['',Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(20),
         Validators.required
       ])]
-    })
+    });
 
-    this.todos.push(new Todo(1, "Comer", true));
-    this.todos.push(new Todo(2, "Viajar", false));
-    this.todos.push(new Todo(3, "Sair", false));
+    this.load()
 
+
+  }
+
+  public add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length +1 ;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  private clear(){
+    this.form.reset()
   }
 
   public removeTodo(todo: Todo) {
     const index = this.todos.indexOf(todo);
-
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save()
   }
 
   public markAsDone(todo: Todo) {
-      todo.done = true
+      todo.done = true;
+      this.save()
   }
 
 
   public markAsUndone(todo: Todo) {
-    todo.done = false
+    todo.done = false;
+    this.save()
+  }
 
+  private save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos',data);
+  }
+
+  private load (){
+    const data = localStorage.getItem('todos') as string;
+
+    const dataParsed = JSON.parse(data);
+
+    return this.todos = dataParsed;
   }
 }
